62f67cac74efb6db70adc79817c6c18f7fcf086f
robert-office <robertuillians123@gmail.com>
2026-01-17 18:46:10 -0300

    feat: hero.

--- src/Components/Hero.vue
+++ src/Components/Hero.vue
@@ -6,13 +6,17 @@ import maos from "@/assets/images/maos.webp"
 
 <template>
 
-  <section class="container mx-auto px-4">
-    <h1 class="text-5xl text-neutral-300 font-medium font-oswald text-center">
+  <section class="flex items-center sm:justify-center justify-start px-4">
+    <button class="sm:mx-auto p-2 text-neutral-300 bg-[#18181b] font-medium cursor-pointer font-roboto">AGENDA LIVRE</button>
+  </section>
+
+  <section class="container mx-auto px-4 mt-4">
+    <h1 class="sm:text-5xl text-4xl text-neutral-300 font-medium font-oswald sm:text-center text-left">
       Transforme sua ideia em realidade <br>com a ajuda de um profissional experiente.
     </h1>
   </section>
 
-  <section>
+  <section class="sm:mt-4 mt-10">
     <img :src="maos" class="w-full" alt="mãos">
   </section>
 
--- src/Components/Topbar.vue
+++ src/Components/Topbar.vue
@@ -3,10 +3,10 @@
 
     <section class="flex items-center gap-6 h-6">
       <p class="text-2xl text-neutral-300 font-light cursor-pointer hover:text-neutral-100 font-gravitas">Portifolio</p>
-      <span class="bg-gray-400 font-bold h-full w-0.5"></span>
+      <span class="sm:block hidden bg-gray-400 font-bold h-full w-0.5"></span>
     </section>
 
-    <section class="flex items-center gap-8 h-6">
+    <section class="items-center gap-8 h-6 sm:flex hidden">
       <p class="text-1xl text-neutral-300 font-medium cursor-pointer hover:text-neutral-100 font-roboto">SOBRE</p>
       <p class="text-1xl text-neutral-300 font-medium cursor-pointer hover:text-neutral-100 font-roboto">SERVIÇOS</p>
       <p class="text-1xl text-neutral-300 font-medium cursor-pointer hover:text-neutral-100 font-roboto">PROJETOS</p>
